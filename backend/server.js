985c9235418f52b98f0cd82ab91331bad6fb664b
bawantha <bawantha@orelit.com>
2022-11-20 08:32:53 +0530

    create server

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const dotemv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//connect to database
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
