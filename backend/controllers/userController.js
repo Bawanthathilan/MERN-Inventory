45daa5398e5cc88c5f654e828fb5ca5f06e77921
bawantha <bawantha@orelit.com>
2022-11-22 12:53:17 +0530

    generate tocken

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -27,6 +34,9 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
   });
 
+  //generate token
+  const token = generateToken(user._id);
+
   if (user) {
     const { _id, name, email, photo, phone, bio } = user;
 
@@ -37,6 +47,7 @@ const registerUser = asyncHandler(async (req, res) => {
       photo,
       phone,
       bio,
+      token,
     });
   } else {
     res.status(400);
