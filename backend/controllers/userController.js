940b0bbca3d1415068318a4385ed7737c35f1256
bawantha <bawantha@orelit.com>
2022-11-22 14:09:13 +0530

    login route added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -8,6 +9,7 @@ const generateToken = (id) => {
   });
 };
 
+//regiater User
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
@@ -64,4 +66,54 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = registerUser;
+//login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  //validate request
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Please fill all fields");
+  }
+
+  //check if user exists
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.status(400);
+    throw new Error("User Not found with this email , Please sign up");
+  }
+
+  //check if password is correct
+  const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+  //generate token
+  const token = generateToken(user._id);
+
+  //send http-only cookie
+  res.cookie("token", token, {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(Date.now() + 1000 * 86400), //1day
+    sameSite: "none",
+    secure: true,
+  });
+
+  if (user && passwordIsCorrect) {
+    const { _id, name, email, photo, phone, bio } = user;
+
+    res.status(200).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+      token,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
+
+module.exports = { registerUser, loginUser };
