ba5e6a2aad35554ce042403160c0e443b4183ac6
bawantha <bawantha@orelit.com>
2022-11-22 14:39:49 +0530

    logout function added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -116,4 +116,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+  });
+
+  return res.status(200).json({
+    message: "Successfully logged out",
+  });
+});
+
+module.exports = { registerUser, loginUser, logoutUser };
