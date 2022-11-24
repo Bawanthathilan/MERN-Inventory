631a434984a909f7a8806597ade21012a696df85
bawantha <bawantha@orelit.com>
2022-11-24 15:19:19 +0530

    add login screem - need to styling

--- frontend/src/pages/auth/Login.js
+++ frontend/src/pages/auth/Login.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./login.scss";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login_container">
+      <div className="form">
+        <div className="form__header">LOGO</div>
+        <div className="form__body">
+          <form>
+            <input type="email" placeholder="Email" className="form__input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="form__input"
+            />
+            <button type="submit" className="form__button">
+              Login
+            </button>
+          </form>
 
-export default Login
+          <a href="/">Home</a>
+          <a href="/forgot">Forgot Password</a>
+          <a href="/register">Register</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
