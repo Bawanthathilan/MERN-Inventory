7f801185292d62d24b90c0b7671c2fcc50e0cd13
bawantha <bawantha@orelit.com>
2022-11-25 00:07:20 +0530

    main layout

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -1,7 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const name = JSON.parse(localStorage.getItem("name"));
+
 const initialState = {
   isLoggedIn: false,
+  name: name ? name : "",
+  user: {
+    name: "",
+    email: "",
+    phone: "",
+    bio: "",
+    photo: "",
+  },
+  userID: "",
 };
 
 const authSlice = createSlice({
