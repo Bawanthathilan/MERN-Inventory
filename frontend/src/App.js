17608ecdb686a558e78d154df0eb3312d47b9d64
bawantha <bawantha@orelit.com>
2022-11-24 14:39:55 +0530

    create auth pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
 
 function App() {
   return (
@@ -7,6 +11,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<ForgotPassword />} />
+          <Route
+            path="/resetpassword/:resetToken"
+            element={<ResetPassword />}
+          />
         </Routes>
       </BrowserRouter>
     </>
