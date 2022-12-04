616580fdce28fce3a9d64e5b853f8b51d70c97f7
bawantha <bawantha@orelit.com>
2022-12-04 18:32:13 +0530

    get all product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,4 +52,12 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
-module.exports = { createProduct };
+//get all products
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({ user: req.user._id }).sort(
+    "-createdAt"
+  );
+  res.status(200).json(products);
+});
+
+module.exports = { createProduct, getProducts };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleWare/authMiddleware");
-const { createProduct } = require("../controllers/productController");
+const {
+  createProduct,
+  getProducts,
+} = require("../controllers/productController");
 const { upload } = require("../utils/fileUpload");
 
 router.post("/", protect, upload.single("image"), createProduct);
+router.get("/", protect, getProducts);
 
 module.exports = router;
